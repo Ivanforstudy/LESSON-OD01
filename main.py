1571ea2bd0e9af4fe7d5259fee5f69e58a0bf256
IvanTusanin <ivantusanin@gmail.com>
2025-05-08 07:24:19 +0300

    ADD

---
+++ main.py
@@ -0,0 +1,17 @@
+def find_max(list):
+    max_number = list[0]
+    for i in list:
+        if i > max_number:
+            max_number = i
+    return max_number
+numbers = [56, 74, 23, 98, 143, -89, -23]
+
+def find_min(list):
+    min_number = list[0]
+    for i in list:
+        if i < min_number:
+            min_number = i
+    return min_number
+numbers = [56, 74, 23, 98, 143, -89, -23]
+print(find_max(numbers))
+print(find_min(numbers))
